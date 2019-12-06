780c119ac2492ac5a2a07bfa1293516f2453bcc2
DenisCalixto <deniscalixto@gmail.com>
2019-12-06 12:12:06 -0800

    Adding subreddits to the list

--- app/src/main/java/edu/wmdd/finalterm/MainActivity.java
+++ app/src/main/java/edu/wmdd/finalterm/MainActivity.java
@@ -2,12 +2,13 @@ package edu.wmdd.finalterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     Button btAdd;
+    EditText subredditText;
     static ArrayList<SubredditItem> itemsArrayList = new ArrayList<SubredditItem>();
     private ListView subredditListView;
     private SubredditListAdapter subredditListAdapter;
@@ -23,13 +25,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SubredditItem item1 = new SubredditItem();
-        item1.name = "pics";
-        itemsArrayList.add(item1);
-        SubredditItem item2 = new SubredditItem();
-        item2.name = "worldnews";
-        itemsArrayList.add(item2);
 
         subredditListView = findViewById(R.id.subredditListView);
         subredditListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
@@ -47,10 +42,16 @@ public class MainActivity extends AppCompatActivity {
         subredditListAdapter = new SubredditListAdapter(this, itemsArrayList);
         subredditListView.setAdapter(subredditListAdapter);
 
+        subredditText = (EditText) findViewById(R.id.subredditText);
+
         btAdd = (Button) findViewById(R.id.btAdd);
         btAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SubredditItem item = new SubredditItem();
+                item.name = subredditText.getText().toString();
+                itemsArrayList.add(item);
+                subredditListAdapter.notifyDataSetChanged();
             }
         });
     }
